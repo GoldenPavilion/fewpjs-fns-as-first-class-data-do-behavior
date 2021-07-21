865d0ed97255966cef40606a94b6095ed8a5088c
GoldenPavilion <rcorystine@gmail.com>
2021-07-20 21:10:51 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(input){
+  const time = input.split(":");
+  const hour = parseInt(time);
+  
+  if (hour < 12) {
+    return "Good Morning"
+  } 
+  else if (hour >= 12 && hour < 17) {
+    return "Good Afternoon"
+  }
+  else if (hour >= 17) {
+    return "Good Evening"    
+  }
+}
+
+function displayMessage(string) {
+  const newHeader = document.getElementById('greeting');
+  newHeader.innerHTML = string;
+}
